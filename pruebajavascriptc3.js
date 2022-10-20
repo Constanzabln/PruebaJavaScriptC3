84428d02f72a649faa6fb55e953a4e1557fedf17
Constanzabln <belensb02@gmail.com>
2022-10-20 11:22:28 -0300

    primera prueba javascript

---
+++ pruebajavascriptc3.js
@@ -0,0 +1,29 @@
+/* Don José todos los martes y jueves realiza un 20% de descuento en el total de las
+compras, crea una función la cual debe recibir un parámetro del día y el arreglo de
+valores de los productos a comprar, esta debe retornar el total final de la compra según
+corresponda o no descuento.
+*/
+
+// mientras sea martes o jueves se le resta un 20% del valor al precio total.
+
+var descuento; diadescuento(martes,jueves);{
+    diadescuento.while (total){
+        
+    }
+    do{
+        (total);--20%
+    }
+
+}
+
+/*Don José hace un 5% de descuento a los clientes que compran más de 3 productos y al
+menos uno de ellos tiene un valor mayor a 10.000, realiza una función que calcule el
+total verificando si corresponde o no el descuento.
+*/
+
+// si sumamos el valor de xproducto mas yproducto mas zproducto, siendo el ultimo con un valor mayor a 10.000
+//se le resta un 5% del valor al precio total.
+
+(x++y++(z>10000))= w--5%{
+
+}
